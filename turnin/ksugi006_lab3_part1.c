a6440765e137cfd3718b9df907b0d5bd71949044
Kiyomi Sugita <ksugi006@wch136-24.cs.ucr.edu>
2020-10-18 12:01:20 -0700

    Completed part 1

--- turnin/ksugi006_lab3_part1.c
+++ turnin/ksugi006_lab3_part1.c
@@ -1,7 +1,7 @@
 /*	Author: ksugi006
  *  Partner(s) Name: 
- *	Lab Section: 024
- *	Assignment: Lab 3  Exercise 1
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -18,24 +18,63 @@ int main(void) {
 	DDRB = 0x00; PORTB = 0XFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char count = 0x00;
-	unsigned char i = 0x01;
-    /* Insert your solution below */
-    while (1) {
-
-	for (i = 0x01; i <= 0x80; i = 2 * i)
-	{
-		if ((PINA & i) == i)
-		{
+	unsigned char tmp = 0x00;
+	while (1) {
+		tmp = PINA & 0x01;
+		if (tmp == 0x01){
 			count = count + 1;
 		}
-		if ((PINB & i) == i)
-		{
+		tmp = PINA & 0x02;
+		if (tmp == 0x02){
 			count = count + 1;
-		}		
+		}
+		tmp = PINA & 0x04;		
+                if (tmp == 0x04){
+                        count = count + 1;
+                }
+		tmp = PINA & 0x08;
+                if (tmp == 0x08){
+                        count = count + 1;
+                }
+		tmp = PINA & 0x10;
+                if (tmp == 0x10){
+                        count = count + 1;
+                }
+		tmp = PINA & 0x20;
+                if (tmp== 0x20){
+                        count = count + 1;
+                }
+		tmp = PINA & 0x40;
+                if (tmp == 0x40){
+                        count = count + 1;
+                }
+		tmp = PINA & 0x80;
+                if (tmp == 0x80){
+                        count = count + 1;
+                }
+                if ((PINB & 0x01) == 0x01){
+                        count = count + 1;
+                }
+                if ((PINB & 0x02) == 0x02){
+                        count = count + 1;
+                }
+                if ((PINB & 0x04) == 0x04){
+                        count = count + 1;
+                }
+                if ((PINB & 0x08) == 0x08){                                                                                                                                                                                                                          count = count + 1;                                                                                                                                                                                                                   }
+                if ((PINB & 0x10) == 0x10){
+                        count = count + 1;
+                }
+                if ((PINB & 0x20) == 0x20){                                                                                                                                                                                                                          count = count + 1;                                                                                                                                                                                                                   }
+                if ((PINB & 0x40) == 0x40){
+                        count = count + 1;
+                }
+                if ((PINB & 0x80) == 0x80){                                                                                                                                                                                                                          count = count + 1;                                                                                                                                                                                                                   }
+                   
+	PORTC = count;
 
 	}
+    /* Insert your solution below */
 
-	PORTC = count;
-    }
     return 1;
 }
