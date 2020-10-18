450914042dc6673dfd909a39882f74258c2e8561
Kiyomi Sugita <ksugi006@wch136-24.cs.ucr.edu>
2020-10-18 14:34:21 -0700

    Completed part 2

--- turnin/ksugi006_lab3_part2.c
+++ turnin/ksugi006_lab3_part2.c
@@ -19,71 +19,63 @@ int main(void) {
 
 	unsigned char level = 0x00;
 	unsigned char tmp = 0x00;
-	unsigned char i = 0x00;
+	unsigned char tmp2 = 0x00;
     /* Insert your solution below */
     
 
 	tmp = 0x00;
-	level = 0x00;
+	level = 0;
+	tmp2 = 0x00;
 
    while (1) {
-	if((PINA & 0x01) == 0x01){
-		level = level  + 1;
+
+	tmp2 = PINA & 0x01;
+	if(tmp2 == 0x01){
+		level = level  | 0x01;
 	}
-        if((PINA & 0x02) == 0x02){
-                level = level  + 2;
+
+	tmp2 = PINA & 0x02;
+        if(tmp2 == 0x02){
+                level = level  | 0x02;
 	}
-	if((PINA & 0x04) == 0x04){
-		level = level + 4;
+
+	tmp2 = PINA & 0x04;
+	if(tmp2 == 0x04){
+		level = level | 0x04;
 	}
-	if((PINA & 0x08) == 0x08) {
-		level = level + 8;
+
+	tmp2 = PINA & 0x08;
+	if(tmp2 == 0x08) {
+		level = level | 0x08;
 	}
 
 	if (level == 0){
 		tmp = 0x00;
 		tmp = tmp | 0x40;
 	}
-	if (level == 1 || level == 2){
+	else if (level <= 2){
 		tmp = 0x00;
 		tmp = tmp | 0x40;
 		tmp = tmp | 0x20;	
 	}
-	if (level == 3 || level == 4){
+	else if (level <= 4){
 		tmp = 0x00;
-		tmp = tmp | 0x40;
-		tmp = tmp | 0x20;
-		tmp = tmp | 0x10;
+		tmp = tmp | 0x70;
 	}
-	if ((level == 5) || (level == 6)){
+	else if (level <= 6){
 		tmp = 0x00;
-		tmp = tmp | 0x20;
-		tmp = tmp | 0x10;
-		tmp = tmp | 0x08;
+		tmp = tmp | 0x38;
 	}
-	if (level == 7 || level == 8 || level == 9){
-		tmp = 0x00;
-		tmp = tmp | 0x20;
-		tmp = tmp | 0x10;
-		tmp = tmp | 0x08;
-		tmp = tmp | 0x04;
+	else if (level <= 9){
+		tmp = tmp | 0x3C;
 	}
-	if (level == 10 || level == 11 || level == 12){
+	else if (level <= 12){
 		tmp = 0x00;
-		tmp = tmp | 0x20;
-		tmp = tmp | 0x10;
-		tmp = tmp | 0x08;
-		tmp = tmp | 0x04;	
-		tmp = tmp | 0x02;
+		tmp = tmp | 0x3E;
 	}
-	if (level == 13 || level == 14 || level == 15) {
+	else {
 		tmp = 0x00;
-		tmp = tmp | 0x20;
-		tmp = tmp | 0x10;
-		tmp = tmp | 0x08;
-		tmp = tmp | 0x04;
-		tmp = tmp | 0x02;
-		tmp = tmp | 0x01;	
+		tmp = tmp | 0x3F;	
 	}
 		
 	PORTC = tmp;
